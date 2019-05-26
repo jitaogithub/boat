8ee44c99c4e7a42511734c0cdaad72c7669ae10b
Tao Ji <jitaoivan@outlook.com>
2019-05-26 15:24:18 +0800

    Typo resolved

--- reset.sh
+++ reset.sh
@@ -32,5 +32,5 @@ docker network rm clipper_network_$node_id >&- 2>&-
 images=`docker images --format '{{.Repository}}\t{{.ID}}' | grep '<none>' | cut -f 2`
 docker image rm $images >&- 2>&-
 
-echo "INFO: [reset.sh] All Clipper $node_id containers, volumns, networks and outdated images are cleared away."
+echo "INFO: [reset.sh] All Clipper $node_id containers, volumes, networks and outdated images are cleared away."
 exit 0
